28ff0a226b211ef0efbe8fd4e05005228645eedf
Jens Kristensen <jens-nk@hotmail.com>
2025-12-16 17:30:21 +0100

    add syntax + converter + simplification rule for red spiders

---
+++ HQPlayground/src/HQP/ZX/Simplify.hs
@@ -0,0 +1,23 @@
+module HQP.ZX.Simplify where
+import HQP.ZX.Syntax
+import HQP.ZX.Utils
+import Algebra.Graph.Undirected
+import Data.List
+
+mergeReds :: ZXDiagram -> ZXDiagram
+mergeReds g =
+    let redSpiders = filter isRed $ vertexList g
+    in mergeRedsRec redSpiders g
+
+mergeRedsRec :: [ZXNode] -> ZXDiagram -> ZXDiagram
+mergeRedsRec [] g = g
+mergeRedsRec (redSpider:redSpiders) g =
+    case partition (\ x -> hasEdge redSpider x g) redSpiders of
+        ([],nonAdjacentReds) -> mergeRedsRec nonAdjacentReds g
+        (redNeighbors,nonAdjacentReds) ->
+            let verticesToMerge = redSpider : redNeighbors
+                newPhase = sum $ map (getPhase . getElement) verticesToMerge
+                (Node v _) = redSpider
+                newSpider = Node v (Red (newPhase))
+                newGraph = mergeVertices (\x -> x `elem` verticesToMerge) newSpider g
+            in mergeRedsRec (newSpider:nonAdjacentReds) newGraph
---
+++ HQPlayground/src/HQP/ZX/Syntax.hs
@@ -0,0 +1,18 @@
+module HQP.ZX.Syntax where
+import Data.Complex
+import Algebra.Graph.Undirected
+
+
+type RealT = Double  -- Can be replaced by e.g. exact fractions or constructive reals
+type ComplexT = Complex RealT
+
+data ZXElement = H | Green RealT | Red RealT | Input | Output | I
+    deriving (Eq,Show)
+data ZXNode = Node Int ZXElement
+    deriving (Eq,Show)
+
+instance Ord ZXNode where
+    (<=) (Node a _) (Node b _) = a <= b
+
+type ZXDiagram = Graph ZXNode
+
