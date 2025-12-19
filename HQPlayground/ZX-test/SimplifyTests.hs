090ea9ed22704b8f2205e0c082a755ce03411d27
Jens Kristensen <jens-nk@hotmail.com>
2025-12-19 15:06:39 +0100

    fix tests

--- HQPlayground/ZX-test/SimplifyTests.hs
+++ HQPlayground/ZX-test/SimplifyTests.hs
@@ -21,29 +21,29 @@ mergeSpiderTests =
     [
         testCase "Merging path of reds between two greens collapses into green-red-green" $
             mergeReds  (path
-                [ Node 0 $ Green pi
-                , Node 1 $ Red pi
-                , Node 2 $ Red pi
-                , Node 3 $ Red pi
-                , Node 4 $ Red pi
-                , Node 5 $ Green pi ])
+                [ Node 0 $ Green (PiHalves 2)
+                , Node 1 $ Red (PiHalves 2)
+                , Node 2 $ Red (PiHalves 2)
+                , Node 3 $ Red (PiHalves 2)
+                , Node 4 $ Red (PiHalves 2)
+                , Node 5 $ Green (PiHalves 2) ])
             @?= (path 
-                [ Node 0 $ Green pi
-                , Node 1 $ Red (4*pi)
-                , Node 5 $ Green pi ]),
+                [ Node 0 $ Green (PiHalves 2)
+                , Node 1 $ Red (PiHalves 0)
+                , Node 5 $ Green (PiHalves 2) ]),
         testCase "Merging in distinct regions" $
             mergeReds  (path
-                [ Node 0 $ Green pi
-                , Node 1 $ Red pi
-                , Node 2 $ Red pi
-                , Node 3 $ Green pi
-                , Node 4 $ Red (pi/2)
-                , Node 5 $ Red (pi/2)
-                , Node 6 $ Green pi ])
+                [ Node 0 $ Green (PiHalves 2)
+                , Node 1 $ Red (PiHalves 2)
+                , Node 2 $ Red (PiHalves 2)
+                , Node 3 $ Green (PiHalves 2)
+                , Node 4 $ Red (PiHalves 1)
+                , Node 5 $ Red (PiHalves 1)
+                , Node 6 $ Green (PiHalves 2) ])
             @?= (path 
-                [ Node 0 $ Green pi
-                , Node 1 $ Red (2*pi)
-                , Node 3 $ Green pi
-                , Node 4 $ Red pi
-                , Node 6 $ Green pi ])
+                [ Node 0 $ Green (PiHalves 2)
+                , Node 1 $ Red (PiHalves 0)
+                , Node 3 $ Green (PiHalves 2)
+                , Node 4 $ Red (PiHalves 2)
+                , Node 6 $ Green (PiHalves 2) ])
     ]
