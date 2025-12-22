4bd43289d1406d7851e39b2b42cb308dfd1563f3
Olipus <olivereriksen@protonmail.com>
2025-12-22 16:57:40 +0100

    remove redundant parenthesis

--- HQPlayground/ZX-test/SimplifyTests.hs
+++ HQPlayground/ZX-test/SimplifyTests.hs
@@ -17,18 +17,18 @@ mergeSpiderTests =
     testGroup "Merge spider tests"
     [
         testCase "Merging path of reds" $
-            mergeRedSpiders  (path
+            mergeRedSpiders (path
                 [ Node 0 $ Green $ PiHalves 2
                 , Node 1 $ Red $ PiHalves 2
                 , Node 2 $ Red $ PiHalves 1
                 , Node 3 $ Red $ PiHalves 2
                 , Node 4 $ Green $ PiHalves 2 ])
-            @?= (path 
+            @?= path 
                 [ Node 0 $ Green $ PiHalves 2
                 , Node 3 $ Red $ PiHalves 1 -- Id 1, 2, 3 could all be correct.
-                , Node 4 $ Green $ PiHalves 2 ]),
+                , Node 4 $ Green $ PiHalves 2 ],
         testCase "Merging paths in distinct regions" $
-            mergeRedSpiders  (path
+            mergeRedSpiders (path
                 [ Node 0 $ Green $ PiHalves 2
                 , Node 1 $ Red $ PiHalves 2
                 , Node 2 $ Red $ PiHalves 2
@@ -36,12 +36,12 @@ mergeSpiderTests =
                 , Node 4 $ Red $ PiHalves 1
                 , Node 5 $ Red $ PiHalves 1
                 , Node 6 $ Green $ PiHalves 2 ])
-            @?= (path 
+            @?= path 
                 [ Node 0 $ Green $ PiHalves 2
                 , Node 1 $ Red $ PiHalves 0
                 , Node 3 $ Green $ PiHalves 2
                 , Node 4 $ Red $ PiHalves 2
-                , Node 6 $ Green $ PiHalves 2 ]),
+                , Node 6 $ Green $ PiHalves 2 ],
         testCase "Merging non paths" $
             mergeRedSpiders  (edges -- TODO: Find a more readable way to construct these test cases
                 [ (Node 0 $ Green $ PiHalves 2, Node 1 $ Red $ PiHalves 1)
@@ -49,9 +49,9 @@ mergeSpiderTests =
                 , (Node 1 $ Red $ PiHalves 1, Node 2 $ Red $ PiHalves 2)
                 , (Node 1 $ Red $ PiHalves 1, Node 3 $ Green $ PiHalves 2) 
                 , (Node 2 $ Red $ PiHalves 2, Node 3 $ Green $ PiHalves 2) ])
-            @?= (edges 
+            @?= edges 
                 [ (Node 0 $ Green $ PiHalves 2, Node 1 $ Red $ PiHalves 3)
-                , (Node 1 $ Red $ PiHalves 3, Node 3 $ Green $ PiHalves 2) ])
+                , (Node 1 $ Red $ PiHalves 3, Node 3 $ Green $ PiHalves 2) ]
     ]
 
 removeSimpleSpiderTests :: TestTree
@@ -63,14 +63,14 @@ removeSimpleSpiderTests =
                 [ Node 0 $ Green $ PiHalves 2
                 , Node 1 $ Red $ PiHalves 0
                 , Node 2 $ Green $ PiHalves 2 ])
-            @?= (path 
+            @?= path 
                 [ Node 0 $ Green $ PiHalves 2
-                , Node 2 $ Green $ PiHalves 2 ]),
+                , Node 2 $ Green $ PiHalves 2 ],
         testCase "Removing multiple zero phase simple spiders" $
             removeSimpleSpiders  (path
                 [ Node 0 $ Green $ PiHalves 2
                 , Node 1 $ Red $ PiHalves 0
                 , Node 2 $ Green $ PiHalves 0])
-            @?= (path 
-                [ Node 0 $ Green $ PiHalves 2 ])
+            @?= path 
+                [ Node 0 $ Green $ PiHalves 2 ]
     ]
