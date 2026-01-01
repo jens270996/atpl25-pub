8c1cc92a6d06daec7609db4be74961200470618f
James Avery <avery@diku.dk>
2026-01-01 18:46:14 +0100

    Added function to compute an operator's support

--- HQPlayground/src/HQP/QOp/HelperFunctions.hs
+++ HQPlayground/src/HQP/QOp/HelperFunctions.hs
@@ -2,12 +2,15 @@ module HQP.QOp.HelperFunctions where
 import HQP.QOp.Syntax
 import Data.Bits(FiniteBits,countTrailingZeros,shiftL,shiftR)
 import Data.List (sort)
+import Data.Set  (union, fromList, toList)
 
 
 -- | Signature of an operator a: C^{2^m} -> C^{2^n} is (m,n) = (op_domain a, op_range a)
 op_qubits :: QOp -> Nat
 op_qubits op = case op of
     Id n    -> n
+    Phase _ -> 0
+    R a _   -> op_qubits a
     C a     -> 1 + op_qubits a
     Tensor    a b -> op_qubits a + op_qubits b
     DirectSum a b -> let  
@@ -36,6 +39,25 @@ step_qubits step = case step of
 prog_qubits :: Program -> Nat
 prog_qubits program = maximum $ map step_qubits program
 
+-- | Support of an operator: the list of qubits it acts non-trivially on.
+op_support :: QOp -> [Nat]
+op_support op = let
+    shift ns k = map (+k) ns
+    union xs ys = toList $ Data.Set.union (fromList xs) (fromList ys)
+  in case op of
+  Id n       -> []
+  Phase _    -> []
+  R _ 0      -> []
+  R a _         -> op_support a
+  C a           -> [0] ++ (op_support a) `shift` 1
+  Tensor a b    -> (op_support a) ++ (op_support b) `shift` (op_qubits a)
+  DirectSum a b -> [0] ++ (union (op_support a) (op_support b) `shift` 1)
+  Compose (Permute ks) b -> permApply ks (op_support b)
+  Compose a (Permute ks) -> invertPerm ks `permApply` (op_support a)
+  Compose a b   -> union (op_support a) (op_support b)
+  Adjoint a     -> op_support a
+  Permute ks    -> permSupport ks
+  _             -> [0] -- 1-qubit gates
 
 
 -- Small helper functions
@@ -59,8 +81,12 @@ evenOdd [] = ([],[])
 evenOdd [x] = ([x],[])
 evenOdd (x:y:xs) = let (es,os) = evenOdd xs in (x:es,y:os)
 
+permApply :: [Int] -> [a] -> [a]
+permApply ks xs = [ xs !! k | k <- ks ]
 
+permSupport :: [Int] -> [Int]
+permSupport ks = [ i | (i,j) <- zip [0..] ks, i /= j ]
 
-invertPerm :: [Int] -> [Int]
+invertPerm :: [Int] -> [Int] -- TODO: invertPerm -> permInvert for consistency
 invertPerm ks = map snd $  -- For each index in the output, find its position in the input
     sort [ (k, i) | (i, k) <- zip [0..] ks ]
